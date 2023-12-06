c73f9289679fdbe240029c33ba9658df733e8a33
SamFerwerda <sam.ferwerda@hotmail.com>
2023-12-06 20:12:55 +0100

    day 4 part 2

--- days/day4.js
+++ days/day4.js
@@ -1,28 +1,66 @@
 const { processLineByLine } = require('../common');
 
-(async function () {
+function removeDuplicates(arr) {
+    return arr.filter((item, index) => arr.indexOf(item) === index);
+}
+
+function getPointsOfGame(str){
+    let points = 0;
+    const gameWinners = str.split(':')[1].split('|')[0].trim().split(' ').map((string)=> eval(string));
+    const gottenNumbers = str.split(':')[1].split('|')[1].trim().split(' ').map((string)=> eval(string));
+
+    let numbersMatched = gameWinners.filter((number)=> number && gottenNumbers.includes(number));
+    numbersMatched = removeDuplicates(numbersMatched);
+
+    if (numbersMatched.length){
+        points += 2**(numbersMatched.length-1);
+    }
+
+    return { matches: numbersMatched.length, points, copies:0}
+}
+
+async function partOne(){
     const lines = (await processLineByLine('scratch'));
     let points = 0;
 
     for (i in lines){
-        const line = lines[i];
-        let numbersMatched = [];
-        const gameWinners = line.split(':')[1].split('|')[0].trim().split(' ').map((str)=> eval(str));
-        const gottenNumbers = line.split(':')[1].split('|')[1].trim().split(' ').map((str)=> eval(str));
-
-        console.log(gameWinners);
-        console.log(gottenNumbers);
-
-        gameWinners.forEach((number)=>{
-            if (number && gottenNumbers.includes(number) && !numbersMatched.includes(number)){
-                numbersMatched.push(number);
-            }
-        });
-
-        if (numbersMatched.length){
-            console.log(`Line ${i}: ${numbersMatched}`);
-            points += 2**(numbersMatched.length-1);
-        }
+        const scoreOfCard = getPointsOfGame(lines[i]);
+        points+= scoreOfCard.points;
     }
     console.log(points);
+}
+
+async function partTwo(){
+    const lines = (await processLineByLine('scratch'));
+    const mappingOfCardScores = {};
+    let numberOfCopies = 0;
+    let originalCards = 0;
+
+    // retrieve all points and matches per card game
+    for (i in lines){
+        const scoreOfCard = getPointsOfGame(lines[i]);
+        mappingOfCardScores[i] = scoreOfCard;
+        originalCards++;
+    }
+
+    // retrieve all won copies
+    for (key in mappingOfCardScores){
+        const matches = mappingOfCardScores[key].matches;
+        const copies = mappingOfCardScores[key].copies;
+
+        for (let j=eval(key)+1; j <= matches+eval(key); j++){
+            mappingOfCardScores[j].copies += 1 + copies;
+
+        }
+
+        numberOfCopies += copies;
+    }
+
+    console.log(numberOfCopies + originalCards);
+}
+
+(async function () {
+    // await partOne();
+
+    await partTwo();
 })();
